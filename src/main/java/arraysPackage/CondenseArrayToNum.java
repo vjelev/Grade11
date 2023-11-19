8ffb81147ccae0f677dd0a9104f2d733af871a6d
vjelev <velizar116@gmail.com>
2023-11-19 21:07:02 +0200

    Problem 7

---
+++ src/main/java/arraysPackage/CondenseArrayToNum.java
@@ -0,0 +1,24 @@
+package arraysPackage;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CondenseArrayToNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(s -> Integer.parseInt(s))
+                .toArray();
+        while (numbers.length > 1) {
+            int[] condensed = new int[numbers.length - 1];
+            for (int i = 0; i < numbers.length - 1; i++) {
+                condensed[i] = numbers[i] + numbers[i + 1];
+            }
+            numbers = condensed;
+        }
+        System.out.println(numbers[0]);
+    }
+}
+
